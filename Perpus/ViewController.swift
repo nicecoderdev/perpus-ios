c49bc8a5e391f8726eb9490bd7e17d060836f466
Dody <dodyrw@gmail.com>
2021-04-08 09:50:02 +0700

    Initial Commit

---
+++ Perpus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Perpus
+//
+//  Created by Dody Wicaksono on 08/04/21.
+//  Copyright © 2021 Dody Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
